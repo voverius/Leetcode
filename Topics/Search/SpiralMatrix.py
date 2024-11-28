20867738fcce8c12408381c5a6c81a18509226d8
voverius <voverius@tuta.io>
2024-11-28 17:50:56 +0100

    Tests: One more spiral solution

--- Topics/Search/SpiralMatrix.py
+++ Topics/Search/SpiralMatrix.py
@@ -57,6 +57,21 @@ def Solution1(matrix):
     return spiral
 
 
+def Solution2(matrix):
+    result = []
+    while matrix:
+        result += matrix.pop(0)
+        if matrix and matrix[0]:
+            for row in matrix:
+                result.append(row.pop())
+        if matrix:
+            result += matrix.pop()[::-1]
+        if matrix and matrix[0]:
+            for row in matrix[::-1]:
+                result.append(row.pop(0))
+    return result
+
+
 if __name__ == "__main__":
     cases = [
         [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]],
@@ -97,7 +112,7 @@ if __name__ == "__main__":
     ]
 
     for case in cases:
-        for idx, solution in enumerate([Solution1]):
+        for idx, solution in enumerate([Solution1, Solution2]):
             output = solution(matrix=deepcopy(case[0]))
             if output != case[1]:
                 print(f'Failed   Solution {idx + 1}')
